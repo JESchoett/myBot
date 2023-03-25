c1f40af2068d4852d8f54e2c66f89c83ac070f1d
JESchoett <81109511+JESchoett@users.noreply.github.com>
2023-03-25 17:50:57 +0100

    Update der "chtotxt" funktion+update des "help"
    The Momentum System was impemented, but there is still a bug on a botch of a roll

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import discord
 import random
-
 import os
-from dotenv import load_dotenv
-
+from dotenv import load_dotenv #zum Aufruf der .env Datei
 from discord.ext import commands
+import argparse #für die argument Beschreibung der commands
 
-import logging
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+#File Import
+from momentum import Momentum
+from help import MyHelp
 
 #get the Bot Token from a .env file
 load_dotenv()
@@ -21,6 +21,8 @@ client = discord.Client(intents=intents)
 
 default_prefixes = ['$']
 JEbot = commands.Bot(command_prefix = default_prefixes, case_insensitive=True, intents=discord.Intents.all())
+JEbot.help_command = MyHelp()
+
 
 @JEbot.event
 async def on_ready():
@@ -38,77 +40,123 @@ async def on_ready():
     await JEbot.change_presence(status=discord.Status.online, activity=activity)
 
 #hello
-@JEbot.command(name="hello", description='greets you', brief='greets you')
+@JEbot.command(name="hello", description='Begrüßt den User', brief='Begrüßt den User')
 async def hello(ctx):
+    """
+    Begrüßt den User
+    """
     await ctx.send('Hello there!')
 
 #id
 @JEbot.command(name="id", description='gibt deine Discord User-ID an', brief='gibt deine Discord User-ID an')
 async def id(ctx):
+    """
+    gibt deine Discord User-ID an
+    """
     await ctx.send(ctx.message.author.id)
 
 #roll
-@JEbot.command(name="roll", description='rolle einen Würfel', brief='rolle einen Würfel')
-async def roll(ctx, dice='d6', amount=1, system=' '):
+@JEbot.command(name="roll", description='Rolle einen Würfel', brief='Rolle einen Würfel')
+async def roll(ctx, dice='d6', amount=1, system=''):
+    """
+    Rolle einen Würfel
+
+    Argumente (= default):
+        - dice = d6 [wie viele Seiten soll der Würfel haben?]
+        - amount = 1 [wie oft soll gewürfelt werden?]
+        - system = "" [wird mit einem bestimmten System gespielt?]
+                   "Scion"  [Scion: Erfolge werden nochmal gewürfelt und bei einem Botch wird Momentum hinzu gefügt]
+    """
+
     rolling = []
     rollingVal = []
     success = 0
     if system.lower() == "scion":
         dice = 'd10'
+        system == "scion"
 
-    for _ in range (0,amount) :
+    for wuerfel in range (0,amount) :
         roll = True
         while roll:
             diceVal = random.randint(1, int(dice[1:]))
             rollingVal.append(diceVal)
-            rolling.append(f"you rolled a: {diceVal}")
+            rolling.append(f"Wurf {wuerfel+1}: {diceVal}")
             if diceVal == 10 and system.lower("scion"):
                 roll = True
             else:
                 roll = False
 
-    if system.lower() == "scion":
+    botch = False
+    if system == "scion":
         success = rollingVal.count(8) + rollingVal.count(9) + rollingVal.count(10)
-        rolling.append(f"number of successes: {success}")
+        rolling.append(f"Erfolge: {success}")
         if success == 0 and 1 in rollingVal:
             rolling.append(f"WOOOP BOTCH Momentum +2")
+            botch = True
 
-    rolling.append(f"you rolled: {len(rollingVal)} times")
 
     returnString = str(rolling)
     returnString = returnString.replace('[', '')
     returnString = returnString.replace(']', '')
     returnString = returnString.replace(',', '\n')
     returnString = returnString.replace("'", '')
+
     await ctx.send(returnString)
+    if botch:
+        await JEbot.get_command('momentum').invoke(ctx, "add", "2")
 
 @JEbot.command(name="chtotxt", description='den Inhalt des Channels in eine TXT schreiben', brief='den Inhalt des Channels in eine TXT schreiben')
-async def chtotxt(ctx):
+async def chtotxt(ctx, limit=10):
+    """
+    den Inhalt des Channels in eine TXT schreiben
+
+    Argumente (= default):
+        - limit = 10 [wie viele Nachrichten soll zurück gegangen werden?]
+
+    """
     #https://stackoverflow.com/questions/63464013/discord-python-export-an-entire-chat-into-txt-file
     filename = f"{ctx.channel.name}.txt"
     with open(filename, "w") as file:
-        async for msg in ctx.channel.history(limit=None):
+        async for msg in ctx.channel.history(limit):
             file.write(f"{msg.clean_content}\n")
-    await ctx.send(file)
+    with open(filename, "r") as file:
+        await ctx.send(file=discord.File(file, filename))
+    os.remove(filename)
 
-sessionMomentum = 0
-spendMomentum = 0
+
+
 #momentum
-@JEbot.command(name="momentum", description='Scion Momentum', brief='Scion Momentum')
+thisSession = Momentum()
+@JEbot.command(name="momentum", description='Scion Momentum System', brief='Scion Momentum')
 async def momentum(ctx, momentumModus="show", amount=1):
-    global sessionMomentum
-    global spendMomentum
+    """
+    Das Scion Momentum System
+    In dem PnP Scion kann momentum für Würfe ausgegeben werden um eine neue Chance zu haben oder mehr Würfel zu verwenden.
 
+    Argumente (= default):
+        - momentumModus = "show" [Wie viel Momentum hat die Party]\n
+                          "add"  [füge Momentum zum Counter hinzu]\n
+                          "sub"  [ziehe Momentum vom Counter ab]\n
+
+    """
+
+    if momentumModus == "spend":
+        returnStr = (f"euer ausgegeben Momentum {thisSession.spendMomentum}")
+        await ctx.send(returnStr)
     if momentumModus == "add":
-        sessionMomentum += amount
-    if momentumModus == "sub" and sessionMomentum < amount:
+        thisSession.sessionMomentum += amount
+    if momentumModus == "sub" and thisSession.sessionMomentum < amount:
         await ctx.send("ich kann nicht mehr Momentum abziehen als ihr habt")
-    elif momentumModus == "sub" and sessionMomentum >= amount:
-        sessionMomentum -= amount
-        spendMomentum += amount
-    returnStr = (f"euer aktuelles Momentum {sessionMomentum}")
+    elif momentumModus == "sub" and thisSession.sessionMomentum >= amount:
+        thisSession.sessionMomentum -= amount
+        thisSession.spendMomentum += amount
+    returnStr = (f"euer aktuelles Momentum {thisSession.sessionMomentum}")
     await ctx.send(returnStr)
 
-
+#ping
+@JEbot.command(name="ping", description='Zeigt den Ping des Bots', brief='Zeigt den Ping des Bots')
+async def ping(ctx):
+  """Returns the latency of the bot."""
+  await ctx.send(f"Pong! {round(JEbot.latency * 1000)}ms")
 
 JEbot.run(TOKEN)
---
+++ momentum.py
@@ -0,0 +1,5 @@
+class Momentum():
+    def __init__(self):
+        super().__init__()
+        self.sessionMomentum = 0
+        self.spendMomentum = 0
